33d9d87d74d668d3f29a70e23f8d2471e1ea1ef5
Hungyuuki <hungyuuki777@gmail.com>
2022-01-03 15:13:30 +0700

    hung1

--- gameOver.js
+++ gameOver.js
@@ -1,11 +1,10 @@
-class Rider {
+class gameOver{
     _x;
     _y;
     _width;
     _height;
     _speed;
     _img;
-
 
     constructor(x, y, width, height, speed, img) {
         this._x = x;
@@ -63,44 +62,11 @@ class Rider {
     set img(value) {
         this._img = value;
     }
-
     moveLeft() {
-        myRider._x -= myRider._speed;
+        this._x -= 10;
     }
 
-    moveUp() {
-        myRider._img = 'img/Ichigo/Rider Jump/Ichigo_jump up.png'
-        myRider._y -= myRider._speed;
-    }
-
-    moveDown() {
-        myRider._img = 'img/Ichigo/Rider Jump/Ichigo_jump down.png'
-        myRider._y += myRider._speed;
-    }
-
-    moveRight() {
-        myRider._x += myRider._speed;
-    }
-
-    upSpeed() {
-        myRider._speed += 7;
-    }
-
-    drawRider(ctx) {
-
-        let image = new Image();
-        image.src = this._img;
-        ctx.drawImage(image, this._x, this._y, this._width, this._height)
-
-    }
-
-    drawRiderPunch() {
-        let image = new Image();
-        image.src = this._img;
-        ctx.drawImage(image, this._x, this._y, this._width, this.height)
-    }
-
-    drawEffect(ctx) {
+    drawGameOver(ctx) {
         let image = new Image();
         image.src = this._img;
         ctx.drawImage(image, this._x, this._y, this._width, this._height)
